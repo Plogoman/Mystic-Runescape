13e060d043be8858798e931ee519f2cc4c1e3ae9
Plogoman <plogomanyt@gmail.com>
2024-04-11 01:40:28 +0200

    Some new Files

--- .mono/temp/obj/ExportDebug/Mystic- Runescape.AssemblyInfo.cs
+++ .mono/temp/obj/ExportDebug/Mystic- Runescape.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mystic- Runescape")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("ExportDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83d6cf1fc494a12931c6ce6fbe66d0470bbc9c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mystic- Runescape")]
