73decadf05b7d30adcfb3cf9357c8a294e3c0d98
Plogoman <plogomanyt@gmail.com>
2024-04-11 17:56:02 +0200

    Slime Enemy AI

--- SlimeEnemy.cs
+++ SlimeEnemy.cs
@@ -9,6 +9,8 @@ public class SlimeEnemy : KinematicBody2D
     private AnimatedSprite Sprite;
     private RayCast2D BottomLeft;
     private RayCast2D BottomRight;
+    private RayCast2D MiddleLeft;
+    private RayCast2D MiddleRight;
     private Vector2 Velocity;
     private const float Gravity = 200.0f;
     private const float Speed = 30.0f;
@@ -19,6 +21,8 @@ public class SlimeEnemy : KinematicBody2D
         Sprite = GetNode<AnimatedSprite>("AnimatedSprite");
         BottomLeft = GetNode<RayCast2D>("RayCastLeft");
         BottomRight = GetNode<RayCast2D>("RayCastRight");
+        MiddleLeft = GetNode<RayCast2D>("RayCastMiddleLeft");
+        MiddleRight = GetNode<RayCast2D>("RayCastMiddleRight");
         Velocity.x = Speed;
     }
 
@@ -30,6 +34,7 @@ public class SlimeEnemy : KinematicBody2D
         {
             Velocity.y = Gravity;
         }
+        
         if (!BottomRight.IsColliding())
         {
             Velocity.x = -Speed;
@@ -40,6 +45,17 @@ public class SlimeEnemy : KinematicBody2D
             Velocity.x = Speed;
             Sprite.FlipH = true;
         }
+        else if (MiddleRight.IsColliding())
+        {
+            Velocity.x = -Speed;
+            Sprite.FlipH = false;
+        }
+        else if (MiddleLeft.IsColliding())
+        {
+            Velocity.x = Speed;
+            Sprite.FlipH = true;
+        }
+        
         if (!Sprite.Playing)
         {
             Sprite.Play("Jump");
