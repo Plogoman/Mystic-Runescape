faaeef77f3fe2a539ffa729c2c79e9ec33adb234
Plogoman <plogomanyt@gmail.com>
2024-04-19 00:52:17 +0200

    Updates

--- MagicController.cs
+++ MagicController.cs
@@ -11,18 +11,12 @@ public class MagicController : Node
 
     private int currentCount;
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public MagicController()
     {
         IceKnife iceKnife = new IceKnife();
         AvSpells.Add(ResourceLoader.Load(iceKnife.ResourcePath) as PackedScene);
         EquippedSpell = AvSpells[0];
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 
     public void CastSpell(bool faceDirection)
     {
