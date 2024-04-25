57c49b78cf6fdb36b64fb49760c048fde744e90f
Mark <marktamergeorge@gmail.com>
2024-04-25 17:02:23 +0200

    added first level and added portal

---
+++ menu.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public class menu : Control
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+    private void _on_Start_pressed()
+    {
+        GetTree().ChangeScene("res://First.tscn");
+    }
+
+    private void _on_Quit_pressed()
+    {
+        GetTree().Quit();
+    }
+}
